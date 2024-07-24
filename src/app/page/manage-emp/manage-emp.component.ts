7c190546dd4854289bbc7fda65c51207fe6fe51b
Sithum <sithumsandaruwan200123@gmail.com>
2024-07-24 13:46:39 +0530

    Edited the manage-emp component

---
+++ src/app/page/manage-emp/manage-emp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-emp',
+  standalone: true,
+  imports: [],
+  templateUrl: './manage-emp.component.html',
+  styleUrl: './manage-emp.component.css'
+})
+export class ManageEmpComponent {
+
+}
