cd6df052bcc4fa9f28eefa4118620e8dc87d6d6e
Sithum <sithumsandaruwan200123@gmail.com>
2024-07-25 00:54:08 +0530

    Add Employee Data transaction completed.View Emplo yees on developing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ManageEmpComponent } from './page/manage-emp/manage-emp.component';
+import { ViewAllEmployeesComponent } from './page/view-all-employees/view-all-employees.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ManageEmpComponent],
+  imports: [RouterOutlet,ManageEmpComponent,ViewAllEmployeesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/page/manage-emp/manage-emp.component.ts
+++ src/app/page/manage-emp/manage-emp.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-manage-emp',
@@ -20,15 +22,20 @@ export class ManageEmpComponent {
     role_id: ""
   }
 
-  constructor(private http:HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   addEmployee() {
-    this.http.post("http://localhost:8080/emp-controller/add-employeee",this.employeeObj).subscribe((data) => {
-      console.log(data);
+    this.http.post("http://localhost:8080/emp-controller/add-employeee", this.employeeObj).subscribe(
+      (data) => {
+        Swal.fire({
+          title: "Employee Added !",
+          text: "You clicked the button!",
+          icon: "success"
+        });
     })
 
     console.log(this.employeeObj);
-    
+
   }
 
 
