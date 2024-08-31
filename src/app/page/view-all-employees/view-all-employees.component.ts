bbc74b7bf46bf737e70fceff19f49cad13633fc2
Sithum <sithumsandaruwan200123@gmail.com>
2024-08-31 14:30:32 +0530

    get the employee details and set to the view-all-employee table

--- src/app/page/view-all-employees/view-all-employees.component.ts
+++ src/app/page/view-all-employees/view-all-employees.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-view-all-employees',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,FormsModule,CommonModule],
   templateUrl: './view-all-employees.component.html',
   styleUrl: './view-all-employees.component.css'
 })
 export class ViewAllEmployeesComponent {
 
+  public employeeList:any;
+
+  constructor(private http: HttpClient) { 
+    this.loadEmployeeTable();
+  }
+
+  loadEmployeeTable() {
+    this.http.get("http://localhost:8080/emp-controller/get-all").subscribe(res => {
+      this.employeeList = res; 
+      console.log(res)
+    });
+  }
 }
