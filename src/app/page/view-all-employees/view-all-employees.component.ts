158928f125816d938a9446b5281271c1eec5913d
Sithum <sithumsandaruwan200123@gmail.com>
2024-09-02 08:54:41 +0530

    delete employee function finished

--- src/app/page/view-all-employees/view-all-employees.component.ts
+++ src/app/page/view-all-employees/view-all-employees.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-all-employees',
@@ -18,8 +19,36 @@ export class ViewAllEmployeesComponent {
     this.loadEmployeeTable();
   }
 
+  deleteEmployee(employee : any){
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.http.delete(`http://localhost:8080/emp-controller/delete-emp/${employee.id}`,{responseType:'text'}).subscribe(res=>{
+          console.log(res);
+          this.loadEmployeeTable();
+        })
+        console.log(employee);
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+      }
+    });
+  
+    
+  }
+
   loadEmployeeTable() {
-    this.http.get("http://localhost:8080/emp-controller/get-all").subscribe(res => {
+    this.http.get(`http://localhost:8080/emp-controller/get-all`).subscribe(res => {
       this.employeeList = res; 
       console.log(res)
     });
